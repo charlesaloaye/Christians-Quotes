df61c83a5b6c8cefe7025c037cdaec5e5b7e0c52
Sedenu Aloaye Charles <aloayecharles@gmail.com>
2020-04-29 13:01:37 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,23 +5,23 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title><?php echo $appName;?></title>
+    <title><?= $appName;?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="./css/style.css?<?php echo rand(); ?>">
+    <link rel="stylesheet" type="text/css" href="./css/style.css?<?= rand(); ?>">
 </head>
 <body>
-    <h1><?php echo $appName;?></h1>
+    <h1><?= $appName;?></h1>
     <div class='quotes-container'>
     &ldquo;
-    <?php echo $quote['quote'];?>
+    <?= $quote['quote'];?>
     <br>
-    <strong>&hyphen; <?php echo $quote['author'];?></strong>
+    <strong>&hyphen; <?= $quote['author'];?></strong>
     &rdquo;
     </div>
     <footer>
-     Design With &hearts; by <a href='//instagram.com/sedenucharles' rel='follow' class='link'><?php echo $developer;?></a> 
+     Design With &hearts; by <a href='//instagram.com/sedenucharles' rel='follow' class='link'><?= $developer;?></a> 
      <br>
-     You can <a href='tel:<?php echo $developer_phone; ?>' class='link'>call Me</a> or <a href='mailto:<?php echo $developer_email; ?>' class='link'>Email Me</a>
+     You can <a href='tel:<?= $developer_phone; ?>' class='link'>call Me</a> or <a href='mailto:<?= $developer_email; ?>' class='link'>Email Me</a>
 
     </footer>
 </body>
