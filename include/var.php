4d93c01ba32f15e6168c12653d17e8a9a50e3869
Sedenu <aloayecharles@gmail.com>
2022-09-17 21:36:18 +0100

    Update var.php

--- include/var.php
+++ include/var.php
@@ -5,9 +5,3 @@
 $appName = 'Christians Quotes';
 # myFullName
 $developer = ucwords('sedenu aloaye charles');
-# myPhone Number
-$developer_phone = '+2348137203803';
-# myEmail Address
-$developer_email = 'aloayecharles@gmail.com';
-
-?>
