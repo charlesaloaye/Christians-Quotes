7993b473279d31a61e5e1918394c642260aae5c8
Sedenu <aloayecharles@gmail.com>
2024-08-26 14:31:19 +0100

    Update var.php

--- include/var.php
+++ include/var.php
@@ -3,4 +3,10 @@
 $app_name = 'Random Christians Quotes';
 
 # Developer name
-$developer = 'Sedenu Charles';
+$dev_name = 'Sedenu Charles';
+
+# Developer Email
+$dev_email = 'aloayecharles@gmail.com';
+
+# Developer Phone
+$dev_phone = '+2348137203803';
