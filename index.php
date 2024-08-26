0ed1c81a607419c77a139e0f997b9a14878e09e0
Sedenu <aloayecharles@gmail.com>
2024-08-26 14:28:40 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,12 +5,12 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title><?= $appName;?></title>
+    <title><?= $app_name;?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="./css/style.css?<?= rand(); ?>">
 </head>
 <body>
-    <h1><?= $appName;?></h1>
+    <h1><?= $app_name;?></h1>
     <div class='quotes-container'>
     &ldquo;
     <?= $quote['quote'];?>
